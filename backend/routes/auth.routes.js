d0df083d15baf4a3ea97b71023e187563fd61ada
Touhid Tamboli <touhidtamboli0314@gmail.com>
2024-04-21 16:09:11 +0530

    initial commit

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -5,4 +5,12 @@ router.get("/signup", (req, res) => {
     res.send('signup route');
 });
 
+router.get("/login", (req, res) => {
+    res.send('signup route');
+});
+
+router.get("/logout", (req, res) => {
+    res.send('signup route');
+});
+
 module.exports = router;
