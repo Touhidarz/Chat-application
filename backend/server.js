8cebd3ddd898724242b577aa1d1a4326aa28dc1f
Touhid Tamboli <touhidtamboli0314@gmail.com>
2024-04-22 09:19:58 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,10 @@ const authRoutes = require("./routes/auth.routes.js");  // File Imports
 const connectToMongoDB = require('./db/connectMongoDB.js');
 
 const app = express(); // Initialize Express app // Variables
+// const PORT = process.env.PORT || 5000;
 const PORT = process.env.PORT || 5000;
 
+// dotenv.config();
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming request with json(from req.body)
