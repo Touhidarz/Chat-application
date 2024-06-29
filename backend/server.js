b3ce0326987d7410bf3a006b56c01165b937dad6
Touhid Tamboli <touhidtamboli0314@gmail.com>
2024-06-29 11:18:21 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cookieParser = require('cookie-parser');
 
 const authRoutes = require("./routes/auth.routes.js");  // File Imports
 const messageRoutes = require("./routes/message.routes.js");  // File Imports
+const userRoutes = require("./routes/user.routes.js");  // File Imports
+
 const connectToMongoDB = require('./db/connectMongoDB.js');
 
 const app = express(); // Initialize Express app // Variables
@@ -15,8 +17,10 @@ dotenv.config();
 
 app.use(express.json()); //to parse the incoming request with json(from req.body)
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //     res.send("Hello application!");
